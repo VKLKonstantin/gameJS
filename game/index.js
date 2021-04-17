17d0ca5bb68cfe57827d7d95f3c4bd4bd541c5bc
Konstantin <kos8533@yandex.ru>
2021-04-17 13:27:00 +0300

    secondCommit

--- game/index.js
+++ game/index.js
@@ -1,68 +1,14 @@
-var $start = document.querySelector('#start')
-var $game = document.querySelector('#game')
-var $time = document.querySelector('#time')
-
-var score = 0
-var isGameStarted = false
+let $start=document.querySelector('#start')
+let $game=document.querySelector('#game')
 
 $start.addEventListener('click', startGame)
-$game.addEventListener('click', handleBoxClick)
 
-
-function startGame() {
-  isGameStarted = true
-  $game.style.backgroundColor = '#fff'
-  $start.classList.add('hide')
-
-  var interval = setInterval(function() {
-    var time = parseFloat($time.textContent)
-    
-    if (time <= 0) {
-      clearInterval(interval)
-      endGame()
-    } else {
-      $time.textContent = (time - 0.1).toFixed(1)
-    }
-  }, 100)
-
-  renderBox()
+function startGame(){
+ $start.classList.add('hide')
+$game.style.backgroundColor = '#fff'
+renderBox()
 }
-
-function endGame() {
-  isGameStarted = false
-}
-
-function handleBoxClick(event) {
-  if (!isGameStarted) {
-    return 
-  }
-
-  if (event.target.dataset.box) {
-    score++
-    renderBox()
-  }
-}
-
-function renderBox() {
-  $game.innerHTML = ''
-  var box = document.createElement('div')
-  var boxSize = getRandom(30, 100)
-  var gameSize = $game.getBoundingClientRect()
-  var maxTop = gameSize.height - boxSize
-  var maxLeft = gameSize.width - boxSize
-
-  box.style.height = box.style.width = boxSize + 'px'
-  box.style.position = 'absolute'
-  box.style.backgroundColor = '#000'
-  box.style.top = getRandom(0, maxTop) + 'px'
-  box.style.left = getRandom(0, maxLeft) + 'px'
-  box.style.cursor = 'pointer'
-  box.setAttribute('data-box', 'true')
-
-  $game.insertAdjacentElement('afterbegin', box)
-
-}
-
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) + min)
+function renderBox(){
+   let box=document.createElement('div')
+   
 }
